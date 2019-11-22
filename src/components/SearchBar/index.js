ede61af42e0bc0a37a99ce05b516fbf657dcabfd
Tomasz Bogdziun <tomasz.bogdziun@24nettbutikk.no>
2019-11-22 17:18:53 +0100

    searchbar styles and main page styles

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -6,15 +6,64 @@ import styled from 'styled-components';
 import messages from './messages';
 
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 56px;
   padding: 0 16px;
+`;
+
+const Form = styled.form`
+  max-width: 640px;
+  width: 640px;
+  height: 30px;
+  display: flex;
+`;
+
+const Button = styled.button`
+  width: 56px;
+  background: rgb(248, 248, 248);
+  border-color: rgb(209, 209, 209);
+  border-left: none;
+  border-radius: 0 2px 2px 0;
+  cursor: pointer;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+    height: 100%;
+    display: inline-block;
+    background: transparent;
+    box-sizing: border-box;
+    padding: 2px 36px 2px 6px;
+    border: none;
+    outline: none;
+`;
+
+const InputWrapper = styled.div` 
+  flex: 1;
+  height: 30px;
+  display: inline-block;
+  box-sizing: border-box;
+  border-radius: 2px 0 0 2px;
+  border: 1px solid rgb(209, 209, 209);
+  box-shadow: inset 0 1px 2px rgba(17, 17, 17, 0.1);
+
+  &:focus-within {
+    border-color: #1c62b9;
+  }
 `;
 
 const SearchBar = () => {
 
   return (
     <Wrapper>
-      <input name="search" type="text" />
+      <Form>
+        <InputWrapper>
+          <SearchInput name="search" type="text" placeholder="Search" />
+        </InputWrapper>
+        <Button>Search</Button>
+      </Form>
     </Wrapper>
   );
 };
--- src/containers/MainPage/index.js
+++ src/containers/MainPage/index.js
@@ -1,13 +1,63 @@
 import React, { Component } from 'react';
-
+import styled from 'styled-components';
+import YoutubePlayer from 'react-youtube';
 import SearchBar from '../../components/SearchBar';
+import Container from '../../components/Container';
+
+const Wrapper = styled.div`
+    width: 100%;
+`;
+
+const StyledContainer = styled(Container) `
+    background: #F9F9F9;
+    flex-direction: row;
+`;
+
+const VideosContainer = styled.div`
+    width: 402px;
+    box-sizing: border-box;
+    padding: 24px 24px 24px 0;
+`;
+
+const MainVideoContainer = styled.div`
+    padding: 24px;
+    flex: 1;
+`;
+
+const PlayerWrapper = styled.div`
+    position: relative;
+    padding-bottom: 56.25%; /* 16:9 */
+    padding-top: 25px;
+    height: 0;
+`
+
+const StyledYoutubePlayer = styled(YoutubePlayer)`
+    
+    position: absolute;
+          top: 0;
+          left: 0;
+          width: 100% !important;
+          height: 100% !important;
+
+
+`;
 
 class MainPage extends Component {
     render() {
         return (
-            <div>
+            <Wrapper>
                 <SearchBar />
-            </div>
+                <StyledContainer>
+                    <MainVideoContainer>
+                        <PlayerWrapper>
+                            <StyledYoutubePlayer videoId="2g811Eo7K8U" />
+                        </PlayerWrapper>
+                    </MainVideoContainer>
+                    <VideosContainer>
+                        
+                    </VideosContainer>
+                </StyledContainer>
+            </Wrapper>
         )
     }
 }
